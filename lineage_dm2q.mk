029a9950965445ecd949fe5265174191fc8753f5
cola2261 <cola2261@proton.me>
2025-12-27 01:21:36 +0500

    dm2q: Update blobs from S916BXXS8DYI3
    Change-Id: I967f645feba12eb5c9143b8f8ad0c4e10d8567e9

--- lineage_dm2q.mk
+++ lineage_dm2q.mk
@@ -17,13 +17,13 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 PRODUCT_NAME := lineage_dm2q
 PRODUCT_DEVICE := dm2q
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-S9160
+PRODUCT_MODEL := SM-S916B
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="dm2qzhx-user 15 AP3A.240905.015.A2 S9160ZHS6DYF1 release-keys" \
-    BuildFingerprint=samsung/dm2qzhx/dm2q:15/AP3A.240905.015.A2/S9160ZHS6DYF1:user/release-keys \
-    DeviceProduct=dm2qzhx \
-    SystemName=dm2qzhx
+    BuildDesc="dm2qxxx-user 15 AP3A.240905.015.A2 S916BXXS8DYI3 release-keys" \
+    BuildFingerprint=samsung/dm2qxxx/dm2q:15/AP3A.240905.015.A2/S916BXXS8DYI3:user/release-keys \
+    DeviceProduct=dm2q \
+    SystemName=dm2qxxx
