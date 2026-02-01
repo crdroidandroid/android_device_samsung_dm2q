b9e71673f6e4ac2495f44f0717f0266c3e50d36c
cola2261 <cola2261@proton.me>
2026-02-01 17:26:01 +0500

    dm2q: Decommonize powerhint
    Change-Id: I97d10fb56e763060361fd90190d624d09506a901

--- device.mk
+++ device.mk
@@ -27,6 +27,10 @@ PRODUCT_PACKAGES += \
     SystemUIResDm2q \
     WifiResTargetDm2q
 
+# Power
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Touch features
 PRODUCT_PACKAGES += \
     vendor.lineage.touch-service.samsung
